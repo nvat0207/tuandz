249862847c7d39b38cea12ec4e22104fd49ad3e2
nvat0207 <96300188+nvat0207@users.noreply.github.com>
2023-05-29 23:55:33 +0700

    Add files via upload

---
+++ do-an-web/js/config.js
@@ -0,0 +1,50 @@
+const USER = {
+    banks: [
+      
+      {
+            name: "MB BANK",
+            logo: "./images/mbbank.png",
+            number: "11112222456789",
+            owner: "NGUYEN HONG ANH DUC"
+        },
+              {
+            name: "BIDV",
+            logo: "./images/bidv.png",
+            number: "62010001446581",
+            owner: "NGUYEN HONG ANH DUC",
+            
+        },
+       
+      
+      {
+            name: "VIETCOMBANK",
+            logo: "./images/vcb.jpg",
+            number: "1032286524",
+            owner: "NGUYEN HONG ANH DUC"
+        },
+      
+      
+      {
+            name: "TP BANK",
+            logo: "./images/tpbank.jpg",
+            number: "00000166098",
+            owner: "NGUYEN HONG ANH DUC"
+        },
+      
+        {
+            name: "MOMO",
+            logo: "./images/momo.jpeg",
+            number: "0935723340",
+            owner: "NGUYỄN HỒNG ANH ĐỨC",
+            fastTransfer: true
+        },
+               {
+            name: "ZALO PAY",
+            logo: "./images/zalopay.png",
+            number: "0935723340",
+            owner: "NGUYỄN HỒNG ANH ĐỨC",
+            
+        },
+       
+    ]
+};
